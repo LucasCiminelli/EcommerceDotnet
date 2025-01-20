99bb9c167509901a8028ffa766847e43cec6b3b5
LucasCiminelli <lucasaciminelli@gmail.com>
2025-01-20 10:33:40 -0300

    working on ISpecification Interface, BaseSpecfication class and SpecificationParams class

---
+++ backend/src/Core/Ecommerce.Application/Specifications/BaseSpecification.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace Ecommerce.Application.Specifications
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+
+        public BaseSpecification()
+        {
+
+        }
+
+        public BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        public Expression<Func<T, bool>>? Criteria { get; }
+
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+
+        public Expression<Func<T, object>>? OrderBy { get; private set; }
+
+        public Expression<Func<T, object>>? OrderByDescending { get; private set; }
+
+        public int Take { get; private set; }
+
+        public int Skip { get; private set; }
+
+        public bool IsPagingEnable { get; private set; }
+
+
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescendingExpression)
+        {
+            OrderByDescending = orderByDescendingExpression;
+        }
+
+        protected void ApplyPagin(int take, int skip)
+        {
+            Take = take;
+            Skip = skip;
+            IsPagingEnable = true;
+        }
+
+        protected void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+
+
+    }
+}

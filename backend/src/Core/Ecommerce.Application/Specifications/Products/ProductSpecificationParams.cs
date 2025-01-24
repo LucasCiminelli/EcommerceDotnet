0cf3ef82058b24ff0958070ce6e67c97875c7d0a
Lucas Ciminelli <lucasaciminelli@gmail.com>
2025-01-24 16:35:45 -0300

    working on productsSpecification and Pagination

---
+++ backend/src/Core/Ecommerce.Application/Specifications/Products/ProductSpecificationParams.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce.Domain;
+
+namespace Ecommerce.Application.Specifications.Products
+{
+    public class ProductSpecificationParams : SpecificationParams
+    {
+        public int? CategoryId { get; set; }
+        public decimal? PrecioMax { get; set; }
+        public decimal? PrecioMin { get; set; }
+        public int? Rating { get; set; }
+        public ProductStatus? Status { get; set; }
+    }
+}

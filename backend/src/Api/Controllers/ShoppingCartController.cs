f6252de71c91e990f7cf567f4afd7f64b8a08dbd
Lucas Ciminelli <lucasaciminelli@gmail.com>
2025-02-08 20:36:48 -0300

    working on ShoppingCart Controller

---
+++ backend/src/Api/Controllers/ShoppingCartController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Ecommerce.Application.Features.ShoppingCarts.Queries.GetShoppingCartById;
+using Ecommerce.Application.Features.ShoppingCarts.Vms;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ShoppingCartController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public ShoppingCartController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id}", Name = "GetShoppingCart")]
+        [ProducesResponseType(typeof(ShoppingCartVm), (int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult<ShoppingCartVm>> GetShoppingCart(Guid id)
+        {
+            var shoppingCartId = id == Guid.Empty ? Guid.NewGuid() : id; //cuando no tenes carritos creados, el primer carrito tenes que mandarlo con valor de Guid.Empty = 00000000-0000-0000-0000-000000000000
+            var query = new GetShoppingCartByIdQuery(shoppingCartId);
+
+
+            return await _mediator.Send(query);
+        }
+
+
+    }
+}

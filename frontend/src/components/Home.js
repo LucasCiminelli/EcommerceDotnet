497dcb1af8510338dda8f493ccca1b09de3cb455
Lucas Ciminelli <lucasaciminelli@gmail.com>
2025-02-15 18:12:13 -0300

    working on priceMin priceMax filter feature

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,19 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import MetaData from "./layout/MetaData";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductsPagination } from "../actions/productAction";
 import { useAlert } from "react-alert";
 import Products from "./products/Products";
 import Pagination from "react-js-pagination";
-import { setPageIndex } from "../slices/productsPaginationSlice";
+import { setPageIndex, updatePrecio } from "../slices/productsPaginationSlice";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
 
 export const Home = () => {
+  const [precio, setPrecio] = useState([1, 10000]);
   const dispatch = useDispatch();
 
   // const { products, loading, error } = useSelector((state) => state.products);
@@ -61,12 +67,45 @@ export const Home = () => {
     dispatch(setPageIndex({ pageIndex: pageNumber }));
   };
 
+  function onChangePrecio(precioEvent) {
+    setPrecio(precioEvent);
+  }
+
+  function onAfterChange(precioEvent) {
+    dispatch(updatePrecio({ precio: precioEvent }));
+  }
+
   return (
     <Fragment>
       <MetaData titulo={"Los mejores productos online"} />
       <section id="products" className="container mt-5">
         <div className="row">
-          <Products col={4} products={products} loading={loading} />
+          {search ? (
+            <React.Fragment>
+              <div className="col-6 col-md-3 mt-5 mb-5">
+                <div className="px-5">
+                  <Range
+                    marks={{ 1: "$1", 10000: "$10000" }}
+                    min={1}
+                    max={10000}
+                    defaultValue={[1, 10000]}
+                    tipFormatter={(value) => `$${value}`}
+                    value={precio}
+                    tipProps={{ placement: "top", visible: true }}
+                    onChange={onChangePrecio}
+                    onAfterChange={onAfterChange}
+                  />
+                </div>
+              </div>
+              <div className="col-6 col-md-9">
+                <div className="row">
+                  <Products col={4} products={products} loading={loading} />
+                </div>
+              </div>
+            </React.Fragment>
+          ) : (
+            <Products col={4} products={products} loading={loading} />
+          )}
         </div>
       </section>
       <div className="d-flex justify-content-center mt-5">
